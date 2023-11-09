ca76873bdcd4d4e438c1e237043dfdecb129d2b7
Ali Tassoit <ali.tassoit@gmail.com>
2023-11-09 12:15:05 +0100

    Insertion nom joueur + affichage du nom

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollToBottom() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth", // Ajoute une animation de défilement fluide
+  });
+}
+
+function afficherNom() {
+  var nom = document.getElementById("nom").value;
+  var affichageNom = document.getElementById("affichageNom");
+  affichageNom.textContent = "Bonjour, " + nom + " !";
+}
